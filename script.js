bdf8449997cb8077286d061e4d18ae4c95565db7
Martialcart <janolav91@hotmail.com>
2025-02-06 15:45:45 +0100

    buttons connected and working

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("btnWork").addEventListener( 'click', () => {
    console.log("work button pressed"); 
 });
+document.getElementById("btnPause").addEventListener( 'click', () => {
+   console.log("pause button pressed"); 
+});
+document.getElementById("btnSettings").addEventListener( 'click', () => {
+   console.log("settings button pressed"); 
+});
