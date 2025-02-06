46063e16a923e40bb92b457e570f39dec1fe95cd
Martialcart <janolav91@hotmail.com>
2025-02-06 16:20:30 +0100

    before trying one button for start stop

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const timerPause = document.getElementById("timerPause");
 const performance = document.getElementById("performance");
 
 let workStartTime = 0;
+let working = false;
+let pausing = false;
 
 
 
 
-document.getElementById("btnWork").addEventListener( 'click', () => {
+document.getElementById("btnWork").addEventListener('click', () => {
+    if (!working) {
+	console.log("click work when not working");
+    }
    console.log("work button pressed"); 
 });
-document.getElementById("btnPause").addEventListener( 'click', () => {
+document.getElementById("btnPause").addEventListener('click', () => {
    console.log("pause button pressed"); 
 });
-document.getElementById("btnSettings").addEventListener( 'click', () => {
+document.getElementById("btnSettings").addEventListener('click', () => {
    console.log("settings button pressed"); 
 });
